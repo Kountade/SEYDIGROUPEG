fb413e21e4302b71b65461431555325ec7286e57
Codons-ensemble <bounamakountagoudiaby@gmail.com>
2026-08-01 18:59:31 +0100

    TRESORERIE  COM

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-1d840b8c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pvklp5rck1o"
+    "revision": "0.cgbl5smfhe8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
